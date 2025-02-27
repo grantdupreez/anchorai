8df442cf1375b655df25bd7709959fbed6ea79b1
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-02-27 10:37:53 +0000

    Update anchorai.py

--- anchorai.py
+++ anchorai.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
 from llama_index.llms.openai import OpenAI
+from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import Settings
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 import hmac
