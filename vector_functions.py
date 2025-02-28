7910b4b38139372fc09951069ffe5608abfcad0f
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-02-28 20:09:56 +0000

    Update vector_functions.py

--- vector_functions.py
+++ vector_functions.py
@@ -64,8 +64,8 @@ def load_document(file_path: str) -> list[Document]:
     elif file_extension == ".md":
         loader = UnstructuredMarkdownLoader(file_path)
     elif file_extension == ".xlsx":
-        loader = UnstructuredExcelLoader(file_path, mode="elements")
-#        loader = UnstructuredExcelLoader(file_path)
+#        loader = UnstructuredExcelLoader(file_path, mode="elements")
+        loader = UnstructuredExcelLoader(file_path)
     else:
         raise ValueError(f"Unsupported file type: {file_extension}")
 
